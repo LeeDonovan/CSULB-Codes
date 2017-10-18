caa07a3d96392cc4614fb925fd1669032c153071
LeeDonovan <32886721+LeeDonovan@users.noreply.github.com>
2017-10-17 17:32:22 -0700

    First Python Program
    Counting from 1- a number between 10-20

---
+++ LeeA4.py
@@ -0,0 +1,14 @@
+keep_going = True
+while (keep_going == True):
+    
+    num = int(input("Please enter number a number between 10-20 \n\t"))
+    if num >= 10 and num <=20:
+         for i in range(num):
+             print(i+1)
+         keep_going = False
+#keep_going = False goes back to while loop making not equal to True stopping loop.
+    else:
+        print("Wrong Number")
+#Goes back to while loop
+    
+
