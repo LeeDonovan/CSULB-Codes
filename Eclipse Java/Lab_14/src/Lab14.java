b5405461da79eed7c21454f78703389bd42e7493
Donovan Lee <32886721+LeeDonovan@users.noreply.github.com>
2018-04-26 21:14:31 -0700

    4-26-18

---
+++ Eclipse Java/Lab_14/src/Lab14.java
@@ -0,0 +1,14 @@
+
+public class Lab14 {
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.print("Star Wars III: Revenge of The Sith\n\n");
+		String firstName = "Obiwan Kenobi";
+		System.out.println(firstName + ":\nYou were the Chosen One! \nIt was said that you would, destroy the Sith, not join them. \nIt was you who would bring balance to the Force, not leave it in Darkness.");
+		
+	
+	}
+
+}
