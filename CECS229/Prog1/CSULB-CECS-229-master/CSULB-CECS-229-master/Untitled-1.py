49e5fad86d18fa3d535545814f781d2648b14081
Donovan Lee <32886721+LeeDonovan@users.noreply.github.com>
2019-03-21 17:32:39 -0700

    Matrix Jupyter

---
+++ CECS229/Prog1/CSULB-CECS-229-master/CSULB-CECS-229-master/Untitled-1.py
@@ -0,0 +1,17 @@
+#%% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
+import os
+try:
+	os.chdir(os.path.join(os.getcwd(), 'CECS229\Prog1\CSULB-CECS-229-master\CSULB-CECS-229-master'))
+	print(os.getcwd())
+except:
+	pass
+
+#%%
+print("Hello World")
+
+#%% [markdown]
+# Welcome
+Hello#%% [markdown]
+# # HI
+# 
+
